c1c2eb0cab66e09a32ccc803766646f456c5ed05
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2023-12-02 11:43:23 +0100

    December 2nd added

--- src/December_1st/CalculateTrebuchet.java
+++ src/December_1st/CalculateTrebuchet.java
@@ -35,55 +35,22 @@ public class CalculateTrebuchet {
     }
 
     private String replaceNumberSubStringWithInt(String line){
-        if(line.contains("one")){
-            if(line.contains("oneight")){
-                line = line.replace("oneight", "18");
-            }
-            if(line.contains("twone")){
-                line = line.replace("twone", "21");
-            }
-            line = line.replace("one", "1");
-        }
-        if(line.contains("two")){
-            if(line.contains("eightwo")){
-                line = line.replace("eightwo", "82");
-            }
-            line = line.replace("two", "2");
-        }
-        if(line.contains("three")){
-            if(line.contains("threeight")){
-                line = line.replace("threeight", "38");
-            }
-            if(line.contains("eighthree")){
-                line = line.replace("eighthree", "83");
-            }
-            line = line.replace("three", "3");
-
-        }
-        if(line.contains("four")){
-            line = line.replace("four", "4");
-        }
-        if(line.contains("five")){
-            if(line.contains("fiveight")){
-                line = line.replace("fiveight", "58");
-            }
-            line = line.replace("five", "5");
-        }
-        if(line.contains("six")){
-            line = line.replace("six", "6");
-        }
-        if(line.contains("seven")){
-            line = line.replace("seven", "7");
-        }
-        if(line.contains("eight")){
-            if(line.contains("nineight")){
-                line = line.replace("nineight", "98");
-            }
-            line = line.replace("eight", "8");
-        }
-        if(line.contains("nine")){
-            line = line.replace("nine", "9");
-        }
+        line = line.replace("oneight", "18");
+        line = line.replace("twone", "21");
+        line = line.replace("one", "1");
+        line = line.replace("eightwo", "82");
+        line = line.replace("two", "2");
+        line = line.replace("threeight", "38");
+        line = line.replace("eighthree", "83");
+        line = line.replace("three", "3");
+        line = line.replace("four", "4");
+        line = line.replace("fiveight", "58");
+        line = line.replace("five", "5");
+        line = line.replace("six", "6");
+        line = line.replace("seven", "7");
+        line = line.replace("nineight", "98");
+        line = line.replace("eight", "8");
+        line = line.replace("nine", "9");
         return line;
     }
 
