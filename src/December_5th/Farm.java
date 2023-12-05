1d5b8a40e1cc54446be2b7402076971941e75f82
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2023-12-05 18:43:22 +0100

    December 5th part 2 solved!

--- src/December_5th/Farm.java
+++ src/December_5th/Farm.java
@@ -6,11 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Farm {
 
-    Path path = Paths.get("src/December_5th/testInput");
+    Path path = Paths.get("src/December_5th/Input");
+    boolean seedFound = false;
 
     public void checkLocationPartOne(){
         try (BufferedReader bf = Files.newBufferedReader(path)){
@@ -46,14 +46,7 @@ public class Farm {
     public void checkLocationPartTwo(){
         try (BufferedReader bf = Files.newBufferedReader(path)){
             double[][] seeds = readSeedsFromFilePartTwo(bf);
-            for(double[] d: seeds){
-                System.out.println(d[0] + " " + d[1]);
-            }
             double[][] seedToSoil = readRatiosFromFile(bf);
-
-            for(double[] d: seeds){
-                System.out.println(d[0] + " " + d[1]);
-            }
             double[][] soilToFertilizer = readRatiosFromFile(bf);
             double[][] fertilizerToWater = readRatiosFromFile(bf);
             double[][] waterToLight = readRatiosFromFile(bf);
@@ -61,45 +54,27 @@ public class Farm {
             double[][] temperatureToHumidity = readRatiosFromFile(bf);
             double[][] humidityToLocation = readRatiosFromFile(bf);
 
-            seeds = calculateValidSoil(seeds, seedToSoil);
-            seeds = calculateValidSoil(seeds, soilToFertilizer);
-            seeds = calculateValidSoil(seeds, fertilizerToWater);
-            seeds = calculateValidSoil(seeds, waterToLight);
-            seeds = calculateValidSoil(seeds, lightToTemperature);
-            seeds = calculateValidSoil(seeds, temperatureToHumidity);
-            seeds = calculateValidSoil(seeds, humidityToLocation);
-
-            double lowestNumber = Double.MAX_VALUE;
-            System.out.printf("%.0f\n", lowestNumber);
+            double counter = 0;
+            double number;
+            while(true){
+                number = counter;
+                number = adjustLocationPartTwo(number, humidityToLocation);
+                number = adjustLocationPartTwo(number, temperatureToHumidity);
+                number = adjustLocationPartTwo(number, lightToTemperature);
+                number = adjustLocationPartTwo(number, waterToLight);
+                number = adjustLocationPartTwo(number, fertilizerToWater);
+                number = adjustLocationPartTwo(number, soilToFertilizer);
+                number = adjustLocationPartTwo(number, seedToSoil);
+                checkIfSeedExists(number, seeds);
+                if(seedFound){
+                    break;
+                }
+                counter++;
+            }
+            System.out.printf("%.0f\n", counter);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private double[][] calculateValidSoil(double[][] seeds, double [][] nextStep) throws IOException {
-        for(int i = 0; i < seeds.length; i++){
-            for(int j = 0; j < nextStep.length; j++){
-                if(seeds[i][0] < nextStep[j][1]){
-                    seeds[i][0] = nextStep[j][1];
-                    break;
-                }
-            }
-            for(int j = 0; j < nextStep.length; j++){
-                double upperBound = nextStep[j][1] + nextStep[j][2];
-                while(seeds[i][1] > nextStep[j][1]){
-                    if(nextStep[j][1] == upperBound){
-                        break;
-                    }
-                    if(seeds[i][1] == nextStep[j][1]){
-                        seeds[i][1] = nextStep[j][1];
-                        break;
-                    }else{
-                        nextStep[j][1]++;
-                    }
-                }
-            }
-        }
-        return seeds;
     }
 
     private double[] readSeedsFromFile(BufferedReader bf) throws IOException {
@@ -111,20 +86,6 @@ public class Farm {
         double[] seeds = new double[seedStrings.length];
         for(int i = 0; i < seeds.length; i++){
             seeds[i] = Double.parseDouble(seedStrings[i]);
-        }
-        return seeds;
-    }
-
-    private double[][] readSeedsFromFilePartTwo(BufferedReader bf) throws IOException {
-        String reader = bf.readLine();
-        bf.readLine();
-        bf.readLine();
-        reader = reader.replace("seeds: ", "");
-        String[] seedStrings = reader.split(" ");
-        double[][] seeds = new double[seedStrings.length/2][2];
-        for(int i = 0; i < seeds.length; i++){
-            seeds[i][0] = Double.parseDouble(seedStrings[i*2]);
-            seeds[i][1] = Double.parseDouble(seedStrings[i*2]) + Double.parseDouble(seedStrings[i*2+1]);
         }
         return seeds;
     }
@@ -159,5 +120,37 @@ public class Farm {
             }
         }
         return seeds;
+    }
+
+    private double[][] readSeedsFromFilePartTwo(BufferedReader bf) throws IOException {
+        String reader = bf.readLine();
+        bf.readLine();
+        bf.readLine();
+        reader = reader.replace("seeds: ", "");
+        String[] seedStrings = reader.split(" ");
+        double[][] seeds = new double[seedStrings.length/2][2];
+        for(int i = 0; i < seeds.length; i++){
+            seeds[i][0] = Double.parseDouble(seedStrings[i*2]);
+            seeds[i][1] = Double.parseDouble(seedStrings[i*2]) + Double.parseDouble(seedStrings[i*2+1]);
+        }
+        return seeds;
+    }
+
+    private double adjustLocationPartTwo(double number, double[][] modifier){
+        for(int i = 0; i < modifier.length; i++){
+            if(number >= modifier[i][0] && number <= (modifier[i][0] + modifier[i][2])){
+                number += (modifier[i][1] - modifier[i][0]);
+                return number;
+            }
+        }
+        return number;
+    }
+
+    private void checkIfSeedExists(double number, double[][] seeds){
+        for (int i = 0; i < seeds.length; i++){
+            if(number > seeds[i][0] && number < seeds[i][1]){
+                seedFound = true;
+            }
+        }
     }
 }
